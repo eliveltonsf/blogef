3d01510d652f9ca7be64a97d1902559b4383b214
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-05-13 21:20:04 -0300

    feat: script para visualização do menu mobile

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
-
-
 const bx = document.querySelector('.bx');
 const menu_mobile = document.querySelector('.menu-mobile');
-const link_mobile = document.querySelectorAll('.link-menu-mobile');
+const link_mobile = document.querySelectorAll('.link-menu-mobile')
 
-console.log(link_mobile);
+bx.addEventListener('click', () => {
+    console.log("test")
+   bx.classList.toggle('active');
+   menu_mobile.classList.toggle('showmenu');
 
-link_mobile.forEach((item) => {
-    item.addEventListener('click', () => {
-        menu_mobile.classList.add('showmenu');
-        console.log(menu_mobile);
-        
+})
+
+link_mobile.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        menu_mobile.classList.remove('showmenu');
+        bx.classList.toggle('active');
     })
 })
-
-//menu_mobile.classList.add('hidemenu');
-
-bx.addEventListener('click', () =>{
-    bx.classList.toggle('activebx');
-
-    menu_mobile.classList.toggle('showmenu');
-    console.log(menu_mobile);
-})
-
